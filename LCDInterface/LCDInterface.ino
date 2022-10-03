e3a709f3dd29430687d836f0aeb273cc1f37c992
Shadikur <me@shadikur.com>
2022-10-03 14:12:59 +0200

    Update Repo

---
+++ LCDInterface/LCDInterface.ino
@@ -0,0 +1,13 @@
+#include <LiquidCrystal.h>
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d6);
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.setCursor(0,0);
+  lcd.print("Arduino is Fun!");
+}
