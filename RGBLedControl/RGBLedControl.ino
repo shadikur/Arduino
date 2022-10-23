44dbf095241523ae715efea57b3b04a05c7ac001
Shadikur <me@shadikur.com>
2022-10-23 12:17:53 +0200

    Update

--- RGBLedControl/RGBLedControl.ino
+++ RGBLedControl/RGBLedControl.ino
@@ -1,64 +1,18 @@
-int redPin = 8;
-int greenPin = 9;
-int bluePin = 10;
-String myColor;
-String msg = "Enter the color of LED you want: ";
+// LEDs must be connected to arduino PWM pins - see board pinout
+#define RED 6     // pin that red led is connected to
+#define GREEN 5   // pin that green led is connected to
+#define BLUE 3    // pin that blue led is connected to
+#define DELAY 20  // internal delay in ms
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
+  // LED connection pins to be set as an output
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(msg);
-  while (Serial.available() == 0) {
-
-  }
-  myColor = Serial.readString();
-  if (myColor == "red" || myColor == "Red") {
-    digitalWrite(redPin, HIGH);
-    digitalWrite(greenPin, LOW);
-    digitalWrite(bluePin, LOW);
-    Serial.println("Red has been turned ON");
-  }
-  else if (myColor == "green" || myColor == "Green") {
-    digitalWrite(redPin, LOW);
-    digitalWrite(greenPin, HIGH);
-    digitalWrite(bluePin, LOW);
-    Serial.println("Green has been turned ON");
-  }
-  else if (myColor == "blue" || myColor == "Blue") {
-    digitalWrite(redPin, LOW);
-    digitalWrite(greenPin, LOW);
-    digitalWrite(bluePin, HIGH);
-    Serial.println("Blue has been turned ON");
-  }
-  else if (myColor == "purple" || myColor == "Purple") {
-    analogWrite(redPin, 255);
-    analogWrite(greenPin, 0);
-    analogWrite(bluePin, 255);
-    Serial.println("Purple has been turned ON");
-  }
-  else if (myColor == "orange" || myColor == "Orange") {
-    analogWrite(redPin, 255);
-    analogWrite(greenPin, 165);
-    analogWrite(bluePin, 0);
-    Serial.println("Orange has been turned ON");
-  }
-  else if (myColor == "cyan" || myColor == "Cyan") {
-    analogWrite(redPin, 0);
-    analogWrite(greenPin, 255);
-    analogWrite(bluePin, 255);
-    Serial.println("Orange has been turned ON");
-  }
-  else {
-    digitalWrite(redPin, LOW);
-    digitalWrite(greenPin, LOW);
-    digitalWrite(bluePin, LOW);
-    Serial.println("Invalid color name");
-  }
+  analogWrite(RED, 0);
+  analogWrite(GREEN, 255);
+  analogWrite(BLUE, 0);
 }
